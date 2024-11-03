29c5b8bfcf8e8a8b86cb16bf395b0beb2b9de9a3
reshmi97 <160597984+reshmi97@users.noreply.github.com>
2024-11-03 11:54:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var random1=Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+random1+".png");
+
+var random2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src","./images/dice"+random2+".png");
+
+if(random1>random2){
+        document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}else if(random1<random2){
+        document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}
+else if(random1==random2){
+        document.querySelector("h1").innerHTML="Draw!";
+}
